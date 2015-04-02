8ed55c1dd497971f1f2bcf1450367bc9e5e92f4e
Samuli Suortti <samuli.suortti@gmail.com>
2015-04-02 15:35:42 +0300

    Start shell only if database exists

--- shell.py
+++ shell.py
@@ -1,5 +1,22 @@
 from IPython.terminal.embed import embed
+from sqlalchemy.exc import OperationalError
 
-from db import *
+from db import engine
 
-embed()
+
+def database_exists():
+    try:
+        engine.connect()
+        return True
+    except OperationalError:
+        print('The database "sqlalchemy_tutorial" did not exist')
+        print('Please run commands below')
+        print('--------------------------------------')
+        print('createdb sqlalchemy_tutorial')
+        print('psql sqlalchemy_tutorial < world.sql')
+        print('--------------------------------------')
+        return False
+
+
+if database_exists():
+    embed()
