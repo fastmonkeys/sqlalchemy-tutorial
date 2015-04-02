885e98ca58cfd7e119403c8b9e287b4b444993fe
Vesa Uimonen <vesa.uimonen@gmail.com>
2015-04-02 16:05:45 +0300

    Define models

--- db.py
+++ db.py
@@ -1,3 +1,11 @@
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 engine = create_engine('postgres://localhost/sqlalchemy_tutorial')
+
+Base = declarative_base()
+
+Session = sessionmaker(bind=engine)
+
+session = Session()
